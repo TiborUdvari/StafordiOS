c091b9b668b1dca96caaafacc4e8521d7e76af78
Tibor Udvari <tibor.udvari@he-arc.ch>
2013-06-04 10:46:14 +0200

    first commit

---
+++ Matchissimo/Matchissimo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchissimo
+//
+//  Created by Tibor Udvari on 6/4/13.
+//  Copyright (c) 2013 Tibor Udvari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchissimo/Matchissimo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchissimo
+//
+//  Created by Tibor Udvari on 6/4/13.
+//  Copyright (c) 2013 Tibor Udvari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
