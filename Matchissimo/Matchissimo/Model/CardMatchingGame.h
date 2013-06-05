86b5b9af4dc5016fc4ab87ce8b0778bbd80ee1d7
Tibor Udvari <tibor.udvari@he-arc.ch>
2013-06-05 15:30:20 +0200

    Hooked up models with controllers, functional game with score

---
+++ Matchissimo/Matchissimo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchissimo
+//
+//  Created by Tibor Udvari on 6/5/13.
+//  Copyright (c) 2013 Tibor Udvari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchissimo/Matchissimo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchissimo
+//
+//  Created by Tibor Udvari on 6/5/13.
+//  Copyright (c) 2013 Tibor Udvari. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
